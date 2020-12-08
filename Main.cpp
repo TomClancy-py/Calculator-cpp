35cc7c56d554609b9a51d92a7b58faf324f6133b
TomClancy-py <tomclancy5544@gmail.com>
2020-12-08 20:14:33 +0530

    Initial commit

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,31 @@
 //calculator
+
+#include <iostream>
+using namespace std;
+int main(){
+  int num1;
+  int num2;
+  string op;
+  cout << "enter first number" << endl;
+  cin >> num1;
+  cout << "enter second number" << endl;
+  cin >> num2;
+  cout << "enter operator" << endl;
+  cin >> op;
+  if(op == "Add"){
+    cout << num1 + num2;
+  }
+  else if(op == "Subtract"){
+    cout << num1 - num2;
+  }
+  else if(op == "Multiple"){
+    cout << num1 * num2;
+  }
+  else if(op == "Divide"){
+      cout << num1 / num2;
+    }
+  else{
+    cout << "Invalid operator please check your spelling" << endl;
+  }
+  return 0;
+}
